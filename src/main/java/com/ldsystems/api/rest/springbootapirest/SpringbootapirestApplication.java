23467881bed84fc142426306560eefb53764d4f9
Luciano <lucianodeciopacheco@gmail.com>
2023-12-03 11:25:27 -0300

    Iniciando Projeto git Local

---
+++ src/main/java/com/ldsystems/api/rest/springbootapirest/SpringbootapirestApplication.java
@@ -0,0 +1,13 @@
+package com.ldsystems.api.rest.springbootapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootapirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootapirestApplication.class, args);
+	}
+
+}
