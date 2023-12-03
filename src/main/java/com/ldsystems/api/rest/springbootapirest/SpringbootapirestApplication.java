174ea914e2941a867adf02dacf88b93dc50bf503
Luciano <lucianodeciopacheco@gmail.com>
2023-12-03 12:13:31 -0300

    Configurando projeto para rodar TomCat e Payara Micro

--- src/main/java/com/ldsystems/api/rest/springbootapirest/SpringbootapirestApplication.java
+++ src/main/java/com/ldsystems/api/rest/springbootapirest/SpringbootapirestApplication.java
@@ -2,10 +2,18 @@ package com.ldsystems.api.rest.springbootapirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.ldsystems.api.rest.springbootapirest.model")
+@EnableTransactionManagement  //Gerenciar transações tudo automaticamente
+@EnableWebMvc // Permitindo trabalhar com WebMVC
+@RestController //Projeto que vai rodar REST
 public class SpringbootapirestApplication extends SpringBootServletInitializer {
 
     @Override
