c9381f08a9aad8f9b1d27bdb0cae2a6d98aee751
Luciano <lucianodeciopacheco@gmail.com>
2023-12-03 11:34:47 -0300

    Ajuste Main Class

--- src/main/java/com/ldsystems/api/rest/springbootapirest/SpringbootapirestApplication.java
+++ src/main/java/com/ldsystems/api/rest/springbootapirest/SpringbootapirestApplication.java
@@ -2,12 +2,19 @@ package com.ldsystems.api.rest.springbootapirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootapirestApplication {
+public class SpringbootapirestApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringbootapirestApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringbootapirestApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootapirestApplication.class, args);
+    }
 
 }
