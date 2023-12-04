2d70c119695b461a1d5896ee578d99c0711b6076
Luciano <lucianodeciopacheco@gmail.com>
2023-12-04 09:57:03 -0300

    Requisição Usuario e List<Usuario> - retornando Json

--- src/main/java/com/ldsystems/api/rest/springbootapirest/controller/IndexController.java
+++ src/main/java/com/ldsystems/api/rest/springbootapirest/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.ldsystems.api.rest.springbootapirest.controller;
 
+import com.ldsystems.api.rest.springbootapirest.model.Usuario;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController //Arquitetura REST
 @RequestMapping(value = "/index")
@@ -15,7 +18,8 @@ public class IndexController {
 
     /**
      * Método para retornar uma mensagem ao receber uma requisição
-     * @param nome (Nome do Usuário)
+     *
+     * @param nome    (Nome do Usuário)
      * @param salario (Salário do Usuário)
      * @return ResponseEntity com a mensagem de saudação
      */
@@ -31,5 +35,37 @@ public class IndexController {
         str.append("Olá, seja bem vindo ").append(nome);
         str.append(" Bora Brasil. Seu salário: ").append(salario == null ? BigDecimal.valueOf(0).toString() : salario.toString());
         return new ResponseEntity(str.toString(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/usuario", produces = "application/json")
+    public ResponseEntity<Usuario> getUsuario() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setLogin("admin");
+        usuario.setSenha("123");
+        usuario.setNome("Administrador Teste");
+
+        return ResponseEntity.ok(usuario);
+    }
+
+    @GetMapping(value = "/usuarios", produces = "application/json")
+    public ResponseEntity<List<Usuario>> getUsuarios() {
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setLogin("admin");
+        usuario.setSenha("123");
+        usuario.setNome("Administrador Teste");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(1L);
+        usuario2.setLogin("padoca");
+        usuario2.setSenha("123");
+        usuario2.setNome("Padoca");
+
+        List<Usuario> listUsuarios = new ArrayList<Usuario>();
+        listUsuarios.add(usuario);
+        listUsuarios.add(usuario2);
+
+        return ResponseEntity.ok(listUsuarios);
     }
 }
