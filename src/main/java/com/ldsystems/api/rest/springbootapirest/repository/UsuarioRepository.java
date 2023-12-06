27f08ab76a6c64f4d4e6f8e975428c91ea3d5807
Luciano <lucianodeciopacheco@gmail.com>
2023-12-06 16:41:35 -0300

    Service para Carregar informações do usuário a logar

--- src/main/java/com/ldsystems/api/rest/springbootapirest/repository/UsuarioRepository.java
+++ src/main/java/com/ldsystems/api/rest/springbootapirest/repository/UsuarioRepository.java
@@ -2,8 +2,13 @@ package com.ldsystems.api.rest.springbootapirest.repository;
 
 import com.ldsystems.api.rest.springbootapirest.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    @Query(value = "select u from Usuario u where u.login = :login")
+    public Usuario findUsuarioByLogin(@Param("login") String login);
 }
---
+++ src/main/java/com/ldsystems/api/rest/springbootapirest/service/UserDetailImplService.java
@@ -0,0 +1,29 @@
+package com.ldsystems.api.rest.springbootapirest.service;
+
+import com.ldsystems.api.rest.springbootapirest.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class UserDetailImplService implements UserDetailsService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserDetails usuario = usuarioRepository.findUsuarioByLogin(username);
+
+        if (usuario == null) {
+            throw new UsernameNotFoundException("Usuário não encontrado.");
+        }
+
+        return new User(usuario.getUsername(), usuario.getPassword(), usuario.isEnabled(), usuario.isAccountNonExpired(), usuario.isCredentialsNonExpired(), usuario.isAccountNonLocked(), usuario.getAuthorities());
+    }
+}
