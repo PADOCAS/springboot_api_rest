b8d561d5be56ef4db2c06407fc474d8cff37a0f4
Luciano <lucianodeciopacheco@gmail.com>
2023-12-06 16:23:28 -0300

    Usuario com UserDetails e vínculo com Role

--- src/main/java/com/ldsystems/api/rest/springbootapirest/model/Usuario.java
+++ src/main/java/com/ldsystems/api/rest/springbootapirest/model/Usuario.java
@@ -1,13 +1,19 @@
 package com.ldsystems.api.rest.springbootapirest.model;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.io.Serializable;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 
+/**
+ * Implementa UserDetails que auxiliar nos tratamentos para usuário no Spring!
+ * Vamos se preocupar com o login, senha e acessos. O restante dos métodos da interface retornamos true!
+ */
 @Entity
-public class Usuario implements Serializable {
+public class Usuario implements UserDetails {
     private static final long serialVersionUID = 1L;
 
     @Id
@@ -26,7 +32,52 @@ public class Usuario implements Serializable {
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Telefone> listTelefone;
 
+    //Dessa forma, está criando uma unique isolado para o role_id, vamos tirar ela manualmente para não atrapalhar!
+    //Deixar no banco apenas uma unique com os 2 campos (CONSTRAINT usuario_role_uk1 UNIQUE (usuario_id, role_id))
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    //Cria tabela de acessos para o usuário (Vai gerar uma tabela usuario_role unindo (role e usuario))
+    @JoinTable(name = "usuario_role",
+            joinColumns = @JoinColumn(name = "usuario_id", referencedColumnName = "id", table = "usuario", foreignKey = @ForeignKey(name = "usuario_role_fk1")),
+            inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id", table = "role", foreignKey = @ForeignKey(name = "usuario_role_fk2")),
+            uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id", "role_id"}, name = "usuario_role_uk1")})
+    private List<Role> listRole;
+
     public Usuario() {
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return getListRole();
+    }
+
+    @Override
+    public String getPassword() {
+        return getSenha();
+    }
+
+    @Override
+    public String getUsername() {
+        return getLogin();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 
     public Usuario(Long id) {
@@ -71,6 +122,14 @@ public class Usuario implements Serializable {
 
     public void setListTelefone(List<Telefone> listTelefone) {
         this.listTelefone = listTelefone;
+    }
+
+    public List<Role> getListRole() {
+        return listRole;
+    }
+
+    public void setListRole(List<Role> listRole) {
+        this.listRole = listRole;
     }
 
     @Override
