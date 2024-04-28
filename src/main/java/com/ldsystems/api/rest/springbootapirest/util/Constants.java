cca97b8fe938da26d3049390bf5bba739d8443eb
Luciano <lucianodeciopacheco@gmail.com>
2024-04-28 16:07:19 -0300

    Troca Endereço para o recuperar senha em produção

--- src/main/java/com/ldsystems/api/rest/springbootapirest/util/Constants.java
+++ src/main/java/com/ldsystems/api/rest/springbootapirest/util/Constants.java
@@ -4,8 +4,8 @@ public class Constants {
 
     public static String getUriRecuperacaoSenhaLinkForEmail() {
         //Para rodar localmente, precisamos por o IP da máquina local (base teste)
-         return "http://localhost:4200/recuperar-senha";
+//         return "http://localhost:4200/recuperar-senha";
         //Para rodar em produção:
-//        return "http://192.168.0.17:8080/";
+        return "https://ldsystems.com.br/angular-rest/recuperar-senha";
     }
 }
